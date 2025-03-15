adca723b4961534772d13964ed2722c4b7fd29de
raja <raj9211@gmail.com>
2025-03-15 23:45:04 +0530

    api key added

--- chrome-extension/chrome-extension-flask/flask_app/models/llm.py
+++ chrome-extension/chrome-extension-flask/flask_app/models/llm.py
@@ -1,6 +1,22 @@
 from flask import request
 import requests
 
+import os
+from dotenv import load_dotenv
+
+
+# Load environment variables
+load_dotenv()
+
+############## gemini search tool ####################
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+gemini_model   = "gemini-2.0-flash"
+
+client      = genai.Client(api_key=gemini_api_key)
+
+
+
+
 class LLMClient:
     def __init__(self, api_key, api_url):
         self.api_key = api_key
@@ -22,7 +38,7 @@ class LLMClient:
             raise Exception(f"Error: {response.status_code}, {response.text}")
 
 def get_llm_summary(text, topics):
-    api_key = "YOUR_API_KEY"  # Replace with your actual API key
+    api_key = gemini_api_key  # Replace with your actual API key
     api_url = "https://api.gemini.example.com/summarize"  # Replace with the actual API endpoint
     llm_client = LLMClient(api_key, api_url)
     return llm_client.summarize_text(text, topics)
