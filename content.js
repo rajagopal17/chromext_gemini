369ef3b56af67fa9758a383548683be0704b6e90
raja <raj9211@gmail.com>
2025-03-16 12:33:24 +0530

    deleted old code

--- content.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.action === 'scrapeText') {
-        let text = document.body.innerText;
-        sendResponse({text: text});
-    }
-});
